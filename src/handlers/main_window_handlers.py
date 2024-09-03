2d5e6da219e43e8010f8d8f96b34bcccfd5c4d6b
Byeongkuk Oh <byeongkukoh@gmail.com>
2024-09-03 22:52:37 +0900

    feat: 초기화 및 파일 삭제 기능 추가

--- src/handlers/main_window_handlers.py
+++ src/handlers/main_window_handlers.py
@@ -5,9 +5,11 @@ from core.data_processing import data_processing, save_dataframe_to_excel
 
 class MainWindowHandlers:
     def connect_signals(self, main_window):
-        main_window.add_file_button.clicked.connect(main_window.add_file)
-        main_window.processing_data_button.clicked.connect(main_window.process_and_save_data)
-        main_window.settings_button.clicked.connect(main_window.open_settings_dialog)
+        main_window.add_file_button.clicked.connect(self.add_file)
+        main_window.processing_data_button.clicked.connect(self.process_and_save_data)
+        main_window.settings_button.clicked.connect(self.open_settings_dialog)
+        main_window.delete_file_button.clicked.connect(self.delete_file)
+        main_window.reset_file_button.clicked.connect(self.reset_file)
         
         
     def add_file(self, main_window):
@@ -24,7 +26,7 @@ class MainWindowHandlers:
         selected_items = main_window.file_list_widget.selectedItems()
         
         if not selected_items:
-            QMessageBox.warning(self, "경고", "처리할 파일을 선택해주세요.")
+            QMessageBox.warning(main_window, "경고", "처리할 파일을 선택해주세요.")
             return
             
         file_path = selected_items[0].text()
@@ -39,8 +41,48 @@ class MainWindowHandlers:
         
         # 데이터프레임을 엑셀로 저장
         save_result = save_dataframe_to_excel(file_dir=file_dir, file_name=file_name, selected_dataframe=selected_skin_temp_dataframe)
-        QMessageBox.information(self, "결과", save_result)
+        QMessageBox.information(main_window, "결과", save_result)
+    
+    
+    def delete_file(self, main_window):
+        # delete file
+        selected_items = main_window.file_list_widget.selectedItems()
         
+        if not selected_items:
+            QMessageBox.warning(main_window, "경고", "삭제할 파일을 선택해주세요.")
+            return
+        
+        for item in selected_items:
+            main_window.file_list_widget.takeItem(main_window.file_list_widget.row(item))
+
+        QMessageBox.information(main_window, "정보", "선택된 파일이 삭제되었습니다.")
+    
+    
+    """
+    TODO: reset_file 메서드만 왜 main_window가 아닌 self로 file_list_widget을 가져와야 오류가 발생지 않는지 알아내기...!
+    - 모든 ui는 main_window에서 생성됨
+    - main_window.widget의 형태로 가져왔는데, reset_file 메서드에서만 이렇게 작동하지 않음
+    - reset_file 메서드에서는 self.widget 형태로 가져와야 작동함 (하지만.. 여기 class에는 file_list_widget이 없는걸..?)
+    """
+    def reset_file(self, main_window):
+        # reset file list
+        if self.file_list_widget.count() > 0:
+            self.file_list_widget.clear()
+            QMessageBox.information(self, "정보", "파일 목록이 초기화되었습니다.")
+        else:
+            QMessageBox.warning(self, "경고", "추가된 파일이 없습니다.")
+        
+    
+    """
+    TODO: reset_file 메서드 문제 해결 후, update_reset_button_state 메서드 만들기
+    """
+    # def update_reset_button_state(self, main_window):
+    #     # update reset button state
+    #     if main_window.file_list_widget.count() > 0:
+    #         main_window.reset_file_button.setEnabled(True)
+    #     else:
+    #         main_window.reset_file_button.setEnabled(False)
+    
     
     def open_settings_dialog(self, main_window):
         # open settings dialog
--- src/ui/main_window_ui.py
+++ src/ui/main_window_ui.py
@@ -6,17 +6,19 @@ class MainWindowUI:
         main_widget = QWidget()
         main_layout = QVBoxLayout()
         
-        # button layout
+        # add file & data processing button layout
         data_processing_button_layout = QHBoxLayout()
         
         # add file button
         main_window.add_file_button = QPushButton("파일 추가")
-        main_window.add_file_button.setFixedSize(250, 30)
+        # main_window.add_file_button.setFixedSize(300, 30)
+        main_window.add_file_button.setFixedHeight(30)
         data_processing_button_layout.addWidget(main_window.add_file_button)
         
-        # processing data button
+        # data processing button
         main_window.processing_data_button = QPushButton("파일 처리 및 저장")
-        main_window.processing_data_button.setFixedSize(250, 30)
+        # main_window.processing_data_button.setFixedSize(300, 30)
+        main_window.processing_data_button.setFixedHeight(30)
         data_processing_button_layout.addWidget(main_window.processing_data_button)
         
         main_layout.addLayout(data_processing_button_layout)
@@ -25,9 +27,28 @@ class MainWindowUI:
         main_window.file_list_widget = QListWidget()
         main_layout.addWidget(main_window.file_list_widget)
         
+        # file delete and reset button layout
+        file_delete_button_layout = QHBoxLayout()
+        
+        # file delete button
+        main_window.delete_file_button = QPushButton("파일 목록 삭제")
+        # main_window.delete_file_button.setFixedSize(300, 30)
+        main_window.delete_file_button.setFixedHeight(30)
+        file_delete_button_layout.addWidget(main_window.delete_file_button)
+        
+        # reset button
+        main_window.reset_file_button = QPushButton("초기화")
+        # main_window.reset_button.setFixedSize(300, 30)
+        main_window.reset_file_button.setFixedHeight(30)
+        # main_window.reset_file_button.setEnabled(False)
+        file_delete_button_layout.addWidget(main_window.reset_file_button)
+        
+        main_layout.addLayout(file_delete_button_layout)
+        
         # settings button
         main_window.settings_button = QPushButton("환경설정")
-        main_window.settings_button.setFixedSize(500, 30)
+        # main_window.settings_button.setFixedSize(600, 30)
+        main_window.settings_button.setFixedHeight(30)
         main_layout.addWidget(main_window.settings_button)
         
         main_widget.setLayout(main_layout)
